c50dcb925802957709a4ceb22ea435443e8402a8
aikosham <77150670+aikosham@users.noreply.github.com>
2021-01-31 18:42:34 +0100

    Update diffusion.m

--- diffusion.m
+++ diffusion.m
@@ -1,5 +1,5 @@
 p = 0.000144;
-q = 0.5;
+q = 0.31;
 m = 6000000;
 num_years = 30 + 10; % 10 years data provided
 years = [1:num_years];
@@ -21,3 +21,21 @@ for i = 10:num_years
 end
 
 plot(I_t)
+
+
+% Plot p with variance
+sigma_p = 1.82e-5
+x = [5e-5:0.001e-4:2.5e-4];
+y = normpdf(x, p, sigma_p); % x, mu, sigma
+plot(x, y)
+
+% Plot q with variance
+sigma_q = 0.0015;
+x = [0.3:0.0001:0.32];
+y = normpdf(x, q, sigma_q);
+plot(x, y);
+alpha = -norminv(0.05);  % interval: 95%
+u = -norminv(alpha/2);
+
+% confidence interval
+ci = [q - u * sigma_q/sqrt(num_years), q + u * sigma_q/sqrt(num_years)]
